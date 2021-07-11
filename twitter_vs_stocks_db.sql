865e11d430992f01536dc42e1d7127eba7d548eb
zkt2018 <79237709+zkt2018@users.noreply.github.com>
2021-07-11 18:42:55 -0400

    Add files via upload

--- twitter_vs_stocks_db.sql
+++ twitter_vs_stocks_db.sql
@@ -1,16 +1,23 @@
 -- Create tables
 CREATE TABLE stock (
-    stock_date DATE NOT NULL,
+    date DATE NOT NULL,
     close_price FLOAT NOT NULL,
     change FLOAT NOT NULL,
-    PRIMARY KEY (stock_id)
+    PRIMARY KEY (date)
 );
 
 CREATE TABLE tweets_text (
-    tweet_date DATE NOT NULL,
+    date DATE NOT NULL,
     text VARCHAR NOT NULL,
-    FOREIGN KEY (stock_date) REFERENCES stock (stock_date)
+    FOREIGN KEY (date) REFERENCES stock (date)
 );
 
 SELECT * FROM stock;
 SELECT * FROM tweets_text;
+
+
+SELECT stock.date,
+    tweets_text.text
+FROM stock
+INNER JOIN tweets_text
+ON stock.date = tweets_text.date;
