4a651e4dd6b17d4ed0f0e12958cc3490ebd3eab5
zkt2018 <79237709+zkt2018@users.noreply.github.com>
2021-07-17 22:19:43 -0400

    Updated SQL queries

--- twitter_vs_stocks_db.sql
+++ twitter_vs_stocks_db.sql
@@ -1,22 +1,21 @@
 -- Create tables
-CREATE TABLE stock (
-  date DATE NOT NULL,
+CREATE TABLE tesla_stocks (
+    date DATE NOT NULL,
 	open FLOAT NOT NULL,
 	high FLOAT NOT NULL,
 	low FLOAT NOT NULL,
 	close FLOAT NOT NULL,
-	adjclose FLOAT NOT NULL,
 	volume REAL NOT NULL,
-  change FLOAT,
-
-  PRIMARY KEY (date)
+    change FLOAT,
+    PRIMARY KEY (date)
 );
 
-CREATE TABLE tweets_text (
-  date DATE NOT NULL,
+
+CREATE TABLE tweets_data_2010_2020 (
+    date DATE NOT NULL,
 	text VARCHAR NOT NULL,
-	like_count INT NOT NULL,
-	quote_count INT NOT NULL,
+	tokenized_text VARCHAR NOT NULL,
+	like_count INT NOT NULL,	
 	reply_count INT NOT NULL,
 	retweet_count INT NOT NULL
 );
@@ -25,11 +24,12 @@ SELECT * FROM stock;
 SELECT * FROM tweets_text;
 
 -- Merge two tables
-SELECT stock.date,
-	tweets_text.text,
-	stock.close,
-	stock.change
+SELECT tesla_stocks.date,
+	tweets_data_2010_2020.tokenized_text,
+	tesla_stocks.close,
+	tesla_stocks.change
 INTO twitter_vs_stocks
-FROM stock
-INNER JOIN tweets_text
-ON stock.date = tweets_text.date;
+FROM tesla_stocks
+INNER JOIN tweets_data_2010_2020
+ON tesla_stocks.date = tweets_data_2010_2020.date;
+
